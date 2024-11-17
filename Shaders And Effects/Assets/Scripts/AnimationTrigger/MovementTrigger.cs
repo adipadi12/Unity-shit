1363aecc17e4b676397e000aedbbe6114e801d49
adipadi12 <ajha4436@gmail.com>
2024-11-18 01:50:15 +0530

    animation working
    positions still need fixing tho. and added a base model. will try to implement base mechanics ASAP

--- Shaders And Effects/Assets/Scripts/AnimationTrigger/MovementTrigger.cs
+++ Shaders And Effects/Assets/Scripts/AnimationTrigger/MovementTrigger.cs
@@ -17,18 +17,37 @@ public class MovementTrigger : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W))
         {
             animator.SetBool("Jump Bool", true);
+            StartCoroutine(JumpAnimation(animator, "Jump Bool", animator.GetCurrentAnimatorClipInfo(0)[0].clip.length));
         }
         if (Input.GetKeyUp(KeyCode.W))
         {
             animator.SetBool("Jump Bool", false);
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    animator.SetBool("Slide Bool", true);
+        //    StartCoroutine( SlideAnimation(animator, "Slide Bool", animator.GetCurrentAnimatorClipInfo(0)[0].clip.length)); // passes size of the animation clip as an argument
+        //}
+        //if (Input.GetKeyUp(KeyCode.S))
+        //{
+        //    animator.SetBool("Slide Bool", false);
+        //}
+        if(Input.GetKeyDown (KeyCode.S))
         {
-            animator.SetBool("Slide Bool", true);
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            animator.SetBool("Slide Bool", false);
+            animator.Play("Slide");
         }
     }
+
+    private IEnumerator SlideAnimation(Animator animator, string boolName, float animationLength)
+    {
+        yield return new WaitForSeconds(animationLength);
+        animator.SetBool(boolName, false);
+    }
+
+    private IEnumerator JumpAnimation(Animator animator, string boolName, float animLen)
+    {
+        yield return new WaitForSeconds(animLen);
+        animator.SetBool(boolName, false);
+    }
+    
 }
