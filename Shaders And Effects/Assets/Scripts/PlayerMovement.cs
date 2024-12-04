65af3c0c5e7d27126f30879af79890b485bca217
adipadi12 <ajha4436@gmail.com>
2024-12-04 18:50:33 +0530

    Basic Walking Implementation upon Rewind Time

--- Shaders And Effects/Assets/Scripts/PlayerMovement.cs
+++ Shaders And Effects/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 5f;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -15,14 +16,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        if (input.x > 0 || -input.x > 0)
-        {
-            rb.velocity = new Vector3(1,0,0) * input.x * 10f;
-            Debug.Log("Pushin");
-        }
-        if (input.z > 0 || -input.z > 0) { 
-            
-            rb.velocity =new Vector3(0,0,1) * input.z * 10f;
-        }
+        
+        rb.MovePosition(rb.position + input * speed * Time.deltaTime);
     }
 }
