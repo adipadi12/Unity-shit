c075279a16fde5be92616e7e822df30c60497f19
adipadi12 <ajha4436@gmail.com>
2024-12-04 19:33:48 +0530

    Crayyz

--- Shaders And Effects/Assets/Scripts/InterviewPep/Shooting.cs
+++ Shaders And Effects/Assets/Scripts/InterviewPep/Shooting.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject bullet;
+    
+    public Transform bulletFiringPoint;
+    [SerializeField]
+    private float bulletSpeed = 10f;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0)){
+            GameObject bulletInstance = Instantiate(bullet, bulletFiringPoint.position, bulletFiringPoint.rotation);
+            bulletInstance.GetComponent<Rigidbody>().velocity = bulletFiringPoint.forward * bulletSpeed;  //direction * speed = velocity
+        }
     }
 }
